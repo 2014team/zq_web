8827755bc3d152a56bc06c044f7565965fdae32d
Administrator <645874679@qq.com>
2020-06-12 00:05:33 +0800

    init

--- src/com/zq/admin/constant/MenuTypeEnum.java
+++ src/com/zq/admin/constant/MenuTypeEnum.java
@@ -1,3 +1,4 @@
+
 package com.zq.admin.constant;
 
 /**
@@ -7,8 +8,27 @@ package com.zq.admin.constant;
  * @date 2020年4月16日 下午3:46:06
  */
 public enum MenuTypeEnum {
+	MENU(0, "菜单"), // 菜单
+	BUTtON(1, "按钮"),// 按钮
+	;
 
-	MENU, // 菜单
-	BUTtON;// 按钮
+	private Integer value;
 
+	private String name;
+
+	private MenuTypeEnum(int value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+
+	public static String getNameByValue(Integer value) {
+		MenuTypeEnum[] menuTypeEnumArr = MenuTypeEnum.values();
+		for (MenuTypeEnum menuTypeEnum : menuTypeEnumArr) {
+			Integer valueEnum = menuTypeEnum.value;
+			if (valueEnum.equals(value)) {
+				return menuTypeEnum.name;
+			}
+		}
+		return null;
+	}
 }
